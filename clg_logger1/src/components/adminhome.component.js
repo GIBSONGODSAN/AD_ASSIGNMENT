ec379b7d83c2bdd0e7bd06b1db009541ff38f38c
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2023-12-08 15:16:25 +0530

    v13.6

--- clg_logger1/src/components/adminhome.component.js
+++ clg_logger1/src/components/adminhome.component.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import AdminUser from './adminUser.component';
+import ActAssign from './adminActAssign.component';
+import NamesDisplay from './adminAttend.component';
 
 const AdminHome = () => {
   return (
     <div>
       <h1>Welcome to Admin Home</h1>
-      <p>This is a dummy page with a navbar</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/adminUser">Admin Users</Link>
+          </li>
+          <li>
+            <Link to="/adminActAssign">Activity Assignment</Link>
+          </li>
+          <li>
+            <Link to="/adminAttend">Attendance</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/adminUser" element={<AdminUser />} />
+        <Route path="/adminActAssign" element={<ActAssign />} />
+        <Route path="/adminAttend" element={<NamesDisplay />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default AdminHome;
